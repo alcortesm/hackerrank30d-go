d20f196756b4fdb96cd2980b6c1064ca85d9455b
Alberto Cortés <alberto@sourced.tech>
2016-09-09 18:22:54 +0200

    clean code day 03

--- 03/main.go
+++ 03/main.go
@@ -6,14 +6,9 @@ import (
 )
 
 func main() {
-	var n int
-	_, err := fmt.Scanf("%d\n", &n)
+	n, err := readInt()
 	if err != nil {
 		log.Fatal(err)
-	}
-
-	if isInvalid(n) {
-		log.Fatalf("input should be >= 1 and <=100, but was %d\n", n)
 	}
 
 	if isWeird(n) {
@@ -23,14 +18,28 @@ func main() {
 	}
 }
 
+func readInt() (int, error) {
+	var n int
+	_, err := fmt.Scanf("%d\n", &n)
+	if err != nil {
+		return 0, err
+	}
+
+	if isInvalid(n) {
+		return 0, fmt.Errorf("invalid input, should be >= 1 and <=100, but was %d\n", n)
+	}
+
+	return n, nil
+}
+
 func isInvalid(n int) bool {
 	return n < 1 || n > 100
 }
 
-func isOdd(n int) bool {
-	return n%2 != 0
-}
-
 func isWeird(n int) bool {
 	return isOdd(n) || (n >= 6 && n <= 20)
+}
+
+func isOdd(n int) bool {
+	return n%2 != 0
 }
